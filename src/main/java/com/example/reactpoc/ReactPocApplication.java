1da881c55ac3fcf4541bcf6e6698f6a2f41874fd
Harry Ho <hho@vertexinc.com>
2019-07-12 11:49:15 -0400

    react-poc with spring

--- src/main/java/com/example/reactpoc/ReactPocApplication.java
+++ src/main/java/com/example/reactpoc/ReactPocApplication.java
@@ -2,20 +2,21 @@ package com.example.reactpoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Random;
 
 @SpringBootApplication
-public class ReactPocApplication {
+public class ReactPocApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactPocApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ReactPocApplication.class, args);
+  }
 
-	@Bean
-	public Random random() {
-		return new Random();
-	}
+  @Bean
+  public Random random() {
+    return new Random();
+  }
 
 }
